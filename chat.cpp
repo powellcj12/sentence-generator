d6bb8fc7ecc189877b6f1509763c71a9bf720b94
Charlie Powell <powellcj12@gmail.com>
2012-04-02 01:20:16 -0400

    setup main loop

--- chat.cpp
+++ chat.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include "grammar.h"
 
 using namespace std;
 
 int main()
 {
+	string input;
+	grammar myGrammar;
+
+	cout << "Please type the name of the grammar file: ";
+	cin >> input;
+
+	myGrammar.create(file);
+
+	cout << "You may now start conversing with the computer!";
+	cin >> input;
+
+	while(input != "quit")
+	{
+		myGrammar.derive(input);
+		cin >> input;
+	}
+
 	return 0;
 }
